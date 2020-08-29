e60543b862384eb9f1565cad3864b72ab7eeae34
BryceCorp <deltafx25@ymail.com>
2020-08-29 17:04:17 +0100

    modded gitignore file

---
+++ build/precache-manifest.e78a16b6fed40b7e36093ca38104b9fa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fdb406c031b50352b752a82a3638fa2",
+    "url": "/weather-react/index.html"
+  },
+  {
+    "revision": "8b582662f509ff076c4a",
+    "url": "/weather-react/static/css/main.dcbf54db.chunk.css"
+  },
+  {
+    "revision": "4a79708fe2bb8d98f2f2",
+    "url": "/weather-react/static/js/2.07144284.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-react/static/js/2.07144284.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b582662f509ff076c4a",
+    "url": "/weather-react/static/js/main.03912833.chunk.js"
+  },
+  {
+    "revision": "978dc9f2a9f9512ae584",
+    "url": "/weather-react/static/js/runtime-main.17cf43c5.js"
+  },
+  {
+    "revision": "c97cb84b74a52edcb496295687e87822",
+    "url": "/weather-react/static/media/cold-bg.c97cb84b.jpg"
+  },
+  {
+    "revision": "635ed87d2cc826fb213057ac40dfaff1",
+    "url": "/weather-react/static/media/warm-bg.635ed87d.jpg"
+  }
+]);
